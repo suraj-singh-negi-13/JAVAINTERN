a87d0342502a9dd3d411a7c92db507cde1c4efa9
Suraj Singh Negi <negis6102@.com>
2025-06-23 19:04:55 +0530

    day1 task is completed.

--- day1/calculator.java
+++ day1/calculator.java
@@ -5,66 +5,63 @@ class calculator{
     static float result=0;
 
     //addition
-    static void addition(float []arr){
-        for(float i:arr){
-            result+=i;
-        }
+    static void addition(float num){
+        result+=num;
     }
 
     //substraction
-    static void substraction(float []arr){
-        for(float i:arr){
-            result-=i;
-        }
+    static void substraction(float num){
+            result-=num;
     }
 
     //multiplication
-    static void multiplication(float []arr){
-        for(float i:arr){
-            result*=i;
-        }
+    static void multiplication(float num){
+            result*=num;
     }
 
     //division
-    static void division(float []arr){
-        for(float i:arr){
-            if(i==0){
+    static void division(float num){
+            if(num==0){
                 System.out.println("can't divide by 0.");
-                result=0;
-                break;
+                return;
             }
-            result/=i;
-        }
+            result/=num;
     }
-
+    static void clear(){
+        result=0;
+    }
     //main function
     public static void main(String[] args){
         Scanner sc=new Scanner(System.in);
+        int ch=0;
         //Operations(addition,subtraction,multiplication,division)
-        System.out.println("\n1.ADDITION");
-        System.out.println("2.SUBSTRACTION");
-        System.out.println("3.MULTIPLICATION");
-        System.out.println("4.DIVISIONN");
-        System.out.print("Choose any operations:");
-        int ch=sc.nextInt();
-        System.out.print("How much number's do you want to insert:");
-        int numbers=sc.nextInt();
-        float []ar=new float[numbers];
-        for(int i=0;i<numbers;i++){
-            ar[i]=sc.nextFloat();
-        } 
-        switch(ch){
-            case 1: addition(ar);
-                break;
-            case 2: substraction(ar);
-                break;
-            case 3: multiplication(ar);
-                break;
-            case 4: division(ar);
-                break;
-            default:
-                System.out.println("Enter correct operation.");
-        }
-        System.out.println("The result is:"+result);
+        do{
+            System.out.print("Enter a number:");
+            float a=sc.nextFloat();
+            System.out.println("\n1.ADDITION");
+            System.out.println("2.SUBSTRACTION");
+            System.out.println("3.MULTIPLICATION");
+            System.out. println("4.DIVISIONN");
+            System.out.println("0.Exit");
+            System.out.println("5.Clear");
+            System.out.print("Choose any operations:");
+            ch=sc.nextInt();
+            switch(ch){
+                case 1: addition(a);
+                   break;
+                case 2: substraction(a);
+                    break;
+                case 3: multiplication(a);
+                    break;
+                case 4: division(a);
+                    break;
+                case 5: clear();
+                    break;
+                default:
+                    System.out.println("Enter correct operation.");
+            }
+            System.out.println("Current result is :"+result);
+        }while(ch!=0);
+            System.out.println("The final result is:"+result);
     }
 }
