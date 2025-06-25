92dd74f5fda4b517dcabc8093ae4320d33b51783
Suraj Singh Negi <negis6102@.com>
2025-06-25 16:39:55 +0530

    starting day 2 work...that is student management system using CLI

---
+++ day2/Student.java
@@ -0,0 +1,16 @@
+//Student Record management System
+/*
+Task 2 :Student Record Management System
+Objective: Create a CLI-based CRUD system for managing student records.
+Tools :Java, VS Code/IntelliJ CE.
+Deliverables: Java program with classes and menu options.
+
+ */
+
+class Student{
+
+
+    public static void main(String[] args){
+
+    }
+}
