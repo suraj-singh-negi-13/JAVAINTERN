b58b9fa98158c1cacc79776e8cefbc229568cc1f
Suraj Singh Negi <negis6102@.com>
2025-06-25 16:45:44 +0530

    now class Student is created and a parameterised constructor is created.

--- day2/Student.java
+++ day2/Student.java
@@ -8,8 +8,15 @@ Deliverables: Java program with classes and menu options.
  */
 
 class Student{
-
-
+    String name="";
+    String id="";
+    int marks=0;
+    Student(String name,String id,int marks){
+        this.name=name;
+        this.id=id;
+        this.marks=marks;
+    }
+    
     public static void main(String[] args){
 
     }
