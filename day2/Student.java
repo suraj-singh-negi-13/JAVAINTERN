a67a845e4be811f23b49a132d9aa633091ab21c4
Suraj Singh Negi <negis6102@.com>
2025-06-27 20:47:26 +0530

    function add student and remove student created successfully.....

--- day2/Student.java
+++ day2/Student.java
@@ -6,8 +6,11 @@ Tools :Java, VS Code/IntelliJ CE.
 Deliverables: Java program with classes and menu options.
 
  */
+import java.util.Scanner;
+import java.util.ArrayList;
 
 class Student{
+    ArrayList<Student> list=new ArrayList<>();
     String name="";
     String id="";
     int marks=0;
@@ -16,8 +19,57 @@ class Student{
         this.id=id;
         this.marks=marks;
     }
-    
-    public static void main(String[] args){
 
+    static void addStudent(id,name,marks){
+        
+        Student ob=new Student(id,name,marks);
+        list.add(ob);        
+    }
+
+    static void removeStudent(String search){
+        for(Student s: list){
+            list.removeIf(s->id.equals(search));
+        }
+    }
+
+    static void updateStudent(){
+
+    }
+
+    static void searchStudent(){
+        
+    }
+
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        do{
+            System.out.println("|||||||||MENU|||||||||");
+            System.out.println("1.Add Student.");
+            System.out.println("2.Remove Student.");
+            System.out.println("3.Update Student.");
+            System.out.println("4.Display Student.");
+            System.out.println("5.Exit");
+            int op=sc.nextInt();
+            switch(op){
+                case 1: String id=sc.nextLine();
+                        String name=sc.nextLine();
+                        int marks=sc.nextInt();
+                        addStudent(id,name,marks);
+                        System.out.println("Student Added successfully...");
+                        break;
+                case 2: removeStudent();
+                        System.out.println("Student removed successfully...");
+                        break;
+                case 3: updateStudent();
+                        System.out.println("Student data updated successfully...");
+                        break;
+                case 4: searchStudent();
+                        break;
+                case 5: System.exit(0);
+                        break;
+                default:
+                        System.out.println("Choose correct option..");
+            }
+        }while(true);
     }
 }
