93a95a613b46d5b4c56ce5225e06cc31ceefe477
Suraj Singh Negi <negis6102@.com>
2025-06-30 13:47:28 +0530

    all empty functions are created..

---
+++ day5/Account.java
@@ -0,0 +1,28 @@
+/*
+Task 5: Bank Account Simulation
+Objective: Simulate basic bank operations using Java OOP.
+Tools :Java, VS Code, Terminal.
+Deliverables: Class-based Java program with transactions.
+ */
+
+public class Account{
+    ArrayList<Account> list=new ArrayList<>();
+    private void addCustomer(){
+
+    }
+
+    private void deposit(float amount){
+            
+        }
+
+    private void withdraw(float amount){
+
+    }
+
+    private void displayAmount(String accNo){
+
+    }
+    public static void main(String[] args){
+        
+    }
+}
