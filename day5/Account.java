1b214162c4b23d25baef951114024c0cb8acf0da
Suraj Singh Negi <negis6102@.com>
2025-06-30 15:26:02 +0530

    all funtions are created...but some functions are not working properly....notworking on that..

--- day5/Account.java
+++ day5/Account.java
@@ -14,9 +14,9 @@ public class Account{
     private String mobno;
     private float amount;
     ArrayList<Account> list=new ArrayList<>();
-    private void addCustomer(){
+    private void addCustomer(Scanner sc){
         Account ob=new Account();
-        newCust(ob);
+        newCust(ob,sc);
         list.add(ob);
 
     }
@@ -24,23 +24,30 @@ public class Account{
     private void deposit(Scanner sc){
         amount=sc.nextFloat();
         accno=sc.nextLine();
-        if(accountExist(accno) && amount>=1){
-            (ob.amount)+=amount;
-            return;
+        for(Account ob:list){
+            if((ob.accno).equals(accno) && amount>=1){
+                (ob.amount)+=amount;
+                System.out.println("Account deposit Successfully...");
+                return;
+            }
         }
-        System.out.println(" deposit minimum 1rs amount....");
+        System.out.println(" wrong account or deposit minimum 1rs amount....");
      }
+
     //Withdraw function is created.....
     private void withdraw(Scanner sc){
-        /*amount=sc.nextFloat();
+        amount=sc.nextFloat();
         accno=sc.nextLine();
-        if(accountExist(accno) && amount>=1 && amount<=ob.amount){
-            (ob.amount)-=amount;
-            System.out.println("Amount withdraw Successfully....");
-            return;
+        for(Account ob:list){
+            if((ob.accno).equals(accno) && amount>=1 && amount<=ob.amount){
+                (ob.amount)-=amount;
+                System.out.println("Amount withdraw Successfully....");
+                return;
+            }
         }
-        System.out.println("account not matched or amount not sufficient...");*/
+        System.out.println("account not matched or amount not sufficient...");
     }
+
     // Display the Account amount information .....
     private void displayAmount(Scanner sc){
         accno=sc.nextLine();
@@ -48,7 +55,7 @@ public class Account{
             System.out.println("Account not exist...");
             return;
         }
-        for(Student ob:list){
+        for(Account ob:list){
             if((ob.accno).equals(accno)){
                 System.out.println("Name:"+ob.name+"\nbalance amount:"+ob.amount);
                 return;
@@ -65,12 +72,13 @@ public class Account{
         return false;
     }
     //new costomer details....
-    private void newCust(Account ob){
+    private void newCust(Account ob,Scanner sc){
         System.out.print("Enter Customer Account number:");
         ob.accno=sc.nextLine();
+        sc.nextLine();
         if(accountExist(ob.accno)){
             System.out.println("Account already exist...Retry....");
-            newCust(ob);
+            newCust(ob,sc);
         }
         System.out.print("Enter Customer name:");
         ob.name=sc.nextLine();
@@ -94,7 +102,7 @@ public class Account{
             System.out.print("Choose option:");
             op=sc.nextInt();
             switch(op){
-                case 1:ac.addCustomer();
+                case 1:ac.addCustomer(sc);
                     break;
                 case 2:ac.deposit(sc);
                     break;
